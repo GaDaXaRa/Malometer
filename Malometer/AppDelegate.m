13bf0d319fefa7fad0e89ee53016f5a39a35e30b
Miguel Santiago Rodríguez <miguel.santiago@kotasoft.com>
2014-07-03 15:53:19 +0200

    Little refactor

--- Malometer/AppDelegate.m
+++ Malometer/AppDelegate.m
@@ -45,31 +45,8 @@
     controller.managedObjectContext = self.managedObjectContext;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Saves changes in the application's managed object context before the application terminates.
     [self saveContext];
 }
 
@@ -79,8 +56,6 @@
     NSManagedObjectContext *managedObjectContext = self.managedObjectContext;
     if (managedObjectContext != nil) {
         if ([managedObjectContext hasChanges] && ![managedObjectContext save:&error]) {
-            // Replace this implementation with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
             NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
             abort();
         }
@@ -101,26 +76,6 @@
     _managedObjectContext.undoManager = [self buildUndoManager];
     return _managedObjectContext;
 }
-
-- (NSUndoManager *)buildUndoManager
-{
-    NSUndoManager *undoManager = [[NSUndoManager alloc] init];
-    undoManager.groupsByEvent = NO;
-    undoManager.levelsOfUndo = 10;
-    return undoManager;
-}
-
-- (NSManagedObjectModel *)managedObjectModel
-{
-    if (_managedObjectModel != nil) {
-        return _managedObjectModel;
-    }
-    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Malometer" withExtension:@"momd"];
-    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
-    return _managedObjectModel;
-}
-
-#pragma mark - Core Data stack
 
 - (NSManagedObjectContext *)backgroundContext {
     if (_backgroundContext != nil) {
@@ -146,6 +101,27 @@
     return _rootContext;
     
 }
+
+#pragma mark - Core Data stack
+
+- (NSUndoManager *)buildUndoManager
+{
+    NSUndoManager *undoManager = [[NSUndoManager alloc] init];
+    undoManager.groupsByEvent = NO;
+    undoManager.levelsOfUndo = 10;
+    return undoManager;
+}
+
+- (NSManagedObjectModel *)managedObjectModel
+{
+    if (_managedObjectModel != nil) {
+        return _managedObjectModel;
+    }
+    NSURL *modelURL = [[NSBundle mainBundle] URLForResource:@"Malometer" withExtension:@"momd"];
+    _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+    return _managedObjectModel;
+}
+
 
 - (NSPersistentStoreCoordinator *)persistentStoreCoordinator
 {
